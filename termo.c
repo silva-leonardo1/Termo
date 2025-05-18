7a02503f0591600b71ad0ab5fd19364a673a91c9
Leonardo <57074184+silva-leonardo1@users.noreply.github.com>
2025-05-18 20:04:47 -0300

    Update termo.c

--- termo.c
+++ termo.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <windows.h>
+#include <ctype.h>
 
 HANDLE hConsole;
 
@@ -15,9 +16,12 @@ void green(char x);
 void reset();
 void seedRandom();
 void init_console_colors();
+void winHistory(int resultado);
+void printHistory();
 
 int vezesJogadas = 0;
 int fileSize = 0;
+char *vitorias[7] = {0};
 char palavraJogo[5];
 
 int main()
@@ -25,15 +29,15 @@ int main()
     count();
     init_console_colors();
     seedRandom();
-
+    
     escolherPalavra();
     char input[5];  
-    printf("TERMO\n===============\n");
-
+    printf("TERMO\n===============\nDigite 1 para ver historico\nou\n");
+    
     printf("Digite sua palavra: ");
     palavraInput(input);
     verificarPos(input, palavraJogo);
-
+    
     printf("\nPressione Enter para sair...");
     while (getchar() != '\n');
     getchar();
@@ -45,6 +49,12 @@ void palavraInput(char palavra[])
     while(!valido){
         do{
             scanf("%s", palavra);
+
+            if(strcmp(palavra, "1") == 0){
+                printHistory(); 
+                exit(1);
+            }    
+
             while(getchar() != '\n');
             if(strlen(palavra) != 5) printf("A palavra precisa conter 5 letras\n-> ");
         }
@@ -108,11 +118,15 @@ void verificarPos(char input[], char palavraJogo[])
         verificarPos(input, palavraJogo);
     }
 
-    else if(strcmp(input, palavraJogo) == 0) printf("\nVoce Ganhou!\n");
+    else if(strcmp(input, palavraJogo) == 0){
+        printf("\nVoce Ganhou!\n"); 
+        winHistory(vezesJogadas);
+        
+    }
 
-    else if(vezesJogadas == 5)
-    {
+    else if(vezesJogadas == 5){
         printf("\nVoce perdeu! A palavra era: %s", palavraJogo);
+        winHistory(6);
     }
 
 }
@@ -134,6 +148,7 @@ void escolherPalavra()
         {
             linha[strcspn(linha, "\n")] = '\0';
             strcpy(palavraJogo, linha);
+            palavraJogo[0] = tolower(palavraJogo[0]);
             break;
         }
         palavraAtual++;
@@ -151,6 +166,84 @@ void count()
     fclose(arquivo);
 }
 
+void winHistory(int resultado)
+{
+    FILE* arquivo = fopen("vitorias.txt", "r");
+    
+    if(arquivo != NULL)
+    {
+        for(int i = 0; i < 7; i++) {
+            fscanf(arquivo, "%d", &vitorias[i]);
+        }
+        fclose(arquivo);
+
+        vitorias[resultado] += 1;
+
+        arquivo = fopen("vitorias.txt", "w");
+        for(int i = 0; i < 7; i++) {
+            fprintf(arquivo, "%d\n", vitorias[i]);
+        }
+        fclose(arquivo);
+    }
+    else
+    {
+        arquivo = fopen("vitorias.txt", "w");
+        for(int i = 0; i < 7; i++) {
+            vitorias[i] = 0;
+            fprintf(arquivo, "0\n");
+        }
+        fclose(arquivo);
+        winHistory(resultado);
+    }
+}
+
+void printHistory()
+{
+    FILE *arquivo = fopen("vitorias.txt", "r");
+    if (arquivo == NULL)
+    {
+        printf("Erro ao abrir o arquivo.\n");
+        return;
+    }
+
+    char linha[100];
+
+    for (int i = 0; i < 7; i++)
+    {
+        if (fgets(linha, sizeof(linha), arquivo) == NULL)
+        {
+            printf("Erro ao ler linha %d do arquivo.\n", i + 1);
+            fclose(arquivo);
+            return;
+        }
+
+        vitorias[i] = malloc(10);
+        if (vitorias[i] == NULL)
+        {
+            printf("Erro de alocaco de memória.\n");
+            fclose(arquivo);
+            return;
+        }
+
+        strcpy(vitorias[i], linha);
+        vitorias[i][strcspn(vitorias[i], "\n")] = '\0';
+    }
+
+    fclose(arquivo);
+
+    for (int i = 0; i < 7; i++)
+    {
+        int win = atoi(vitorias[i]);
+        (i != 6) ? printf("%d. ", i + 1) : printf("Mortes. ");
+        for (int j = 0; j < win; j++)
+        {
+            printf("=");
+        }
+        printf("\n");
+        free(vitorias[i]);
+        vitorias[i] = NULL;
+    }
+}
 
 void init_console_colors() {
     hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
